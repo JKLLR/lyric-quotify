969a1f25e807059e38179eae3b3c2cb93b948c51
JKLLR <jeffhuria@gmail.com>
2022-04-10 11:50:32 +0300

    add quotes array

--- src/app/component/quotes/quotes.component.ts
+++ src/app/component/quotes/quotes.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Quoteclass } from 'src/app/quoteclass';
+
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.component.html',
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+  
+  quotes: Quoteclass[] = [ 
+    new Quoteclass('A friend is someone who knows all about you and still loves you.', 'Elbert Hubbard', 'Lizbeth Wangu',  0, 0, new Date(2020, 12, 5)),
+    new Quoteclass('If you can make a woman laugh, you can make her do anything.', 'Marilyn Monroe', 'Lyn Adelaide',  0, 0, new Date(2020, 12, 5)),
+   new Quoteclass('I have no notion of loving people by halves, it is not my  nature.', 'Jane Austen', 'Edrea Leslie',  0, 0, new Date(2020, 12, 5)),
+   new Quoteclass('It is not a lack of love, but a lack of friendship that makes unhappy marriages.', 'Friedrich Nietzsche', 'Charlene Rose',  0, 0, new Date(2020, 12, 5)),
+   new Quoteclass('The truth is, everyone is going to hurt you. You just got to find the ones worth suffering for.', ' Bob Marley', 'Albert Austen',  0, 0, new Date(2020, 12, 5)),
+   
+  ];
+
+  
 
   constructor() { }
 
--- src/app/quoteclass.ts
+++ src/app/quoteclass.ts
@@ -1,2 +1,15 @@
 export class Quoteclass {
+
+    showDescription:boolean;
+    constructor(
+        public Quote: string, 
+        public author: string,
+        public submitter: string,
+        
+        public upVote:number,
+        public downVote:number,
+        public submitDate: Date
+    ){
+        this.showDescription =false;
+    }
 }
