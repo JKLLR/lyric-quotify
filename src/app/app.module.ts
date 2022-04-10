3e49c7d865141ebbf7c3fb83cba0856858c5a09b
JKLLR <jeffhuria@gmail.com>
2022-04-10 03:42:59 +0300

    add quoteclass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
+import { QuotesComponent } from './component/quotes/quotes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    QuotesComponent
   ],
   imports: [
     BrowserModule,
