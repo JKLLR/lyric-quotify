bc069b8059b94b871ab917a801e3ec5e48931d42
JKLLR <jeffhuria@gmail.com>
2022-04-11 05:38:40 +0300

    added pipes ts

--- src/app/pipes.pipe.ts
+++ src/app/pipes.pipe.ts
@@ -1,12 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'pipes'
+  name: 'passedDays'
 })
-export class PipesPipe implements PipeTransform {
+export class passedDays implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value:any) :number{
+
+    let today:any = new Date();
+    let dateDifference = Math.abs(Date.parse(today)-Date.parse(value))*0.001; //convert milliseconds to seconds
+    let differenceInDays= dateDifference/(60*60*24);  //get time in days
+    return Math.round(differenceInDays);
+
   }
 
 }
+
+
