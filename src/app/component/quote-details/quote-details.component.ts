f12dc437753e5dac6f4034630477948b9c7cf7d6
JKLLR <jeffhuria@gmail.com>
2022-04-11 04:53:02 +0300

    add html and ts to quote-details

--- src/app/component/quote-details/quote-details.component.ts
+++ src/app/component/quote-details/quote-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quoteclass } from 'src/app/quoteclass';
 
 @Component({
   selector: 'app-quote-details',
@@ -6,6 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
+  
+  @Input() quote!: Quoteclass;
+
+// delete function event-emitter
+ @Output() toDelete= new EventEmitter<boolean>();
+
+ deleteQuote (Delete:boolean) {
+   this.toDelete.emit(Delete)
+ }
+
+ //upvote function event emitter
+
+ @Output() upThisVote= new EventEmitter<boolean>();
+
+ upTheVote(up:boolean) {
+   this.upThisVote.emit(up)
+ }
+
+// event emitter for downVote
+
+@Output() downThisVote = new EventEmitter<boolean>();
+
+  downTheVote(down:boolean) {
+    this.downThisVote.emit(down)
+  }
 
   constructor() { }
 
