c9cca4a6f0a5852913ef59c2ca867f64c1b128cf
JKLLR <jeffhuria@gmail.com>
2022-04-11 17:29:18 +0300

    add some style

--- src/app/component/quotes/quotes.component.ts
+++ src/app/component/quotes/quotes.component.ts
@@ -10,14 +10,14 @@ import { Quoteclass } from 'src/app/quoteclass';
 export class QuotesComponent implements OnInit {
   
   quotes: Quoteclass[] = [ 
-    new Quoteclass('Emancipate yourselves from mental slavery, None but ourselves can free our minds.', 'Bob Marley', 'Joe Karanja',  0, 0, new Date(2020, 12, 5)),
-    new Quoteclass('The story of life is quicker than the wink of an eye, the story of love is hello and goodbye...until we meet again', ' Jimi Hendrix', 'Owen',  0, 0, new Date(2020, 12, 5)),
-    new Quoteclass("The game aint always fair and that is the thing though. You can play your heart out, everybody do not get a ring though.", 'Drake', 'Mburu',  0, 0, new Date(2020, 12, 5)),
-    new Quoteclass('Ahhh,  finesse If I broke na my business', 'Pheelz', 'Anonymous',  0, 0, new Date(2020, 12, 5)),
-    new Quoteclass('You have every right to a beautiful life.', 'Selena Gomez', 'Austin Obim',  0, 0, new Date(2020, 12, 5)),
-    new Quoteclass('And anytime you feel the pain refrain. Do not carry the world upon your shoulders. For well you know that it iss a fool who plays it cool by making his world a little colder.', 'The Beatles', 'Martin Kimani',  0, 0, new Date(2020, 12, 5)),
-    new Quoteclass('The truth is, everyone is going to hurt you. You just got to find the ones worth suffering for.', ' Bob Marley', 'Derrick Maina',  0, 0, new Date(2020, 12, 5)),
-    new Quoteclass('You have to hurt in order to know. Fall in order to grow. Lose in order to gain. Because most of lifes lessons are learned in pain.', 'J Cole', 'Huria',  0, 0, new Date(2020, 12, 5)),
+    new Quoteclass('Emancipate yourselves from mental slavery, None but ourselves can free our minds.', 'Bob Marley', 'Joe Karanja',  0, 0, new Date(2022, 2, 2)),
+    new Quoteclass('The story of life is quicker than the wink of an eye, the story of love is hello and goodbye...until we meet again', ' Jimi Hendrix', 'Owen',  0, 0, new Date(2022, 4, 4)),
+    new Quoteclass("The game aint always fair and that is the thing though. You can play your heart out, everybody do not get a ring though.", 'Drake', 'Mburu',  0, 0, new Date(2022, 1, 1)),
+    new Quoteclass('Ahhh,  finesse If I broke na my business', 'Pheelz', 'Anonymous',  0, 0, new Date(2022, 3, 3)),
+    new Quoteclass('You have every right to a beautiful life.', 'Selena Gomez', 'Austin Obim',  0, 0, new Date(2022, 2, 14)),
+    new Quoteclass('And anytime you feel the pain refrain. Do not carry the world upon your shoulders. For well you know that it iss a fool who plays it cool by making his world a little colder.', 'The Beatles', 'Martin Kimani',  0, 0, new Date(2022, 4, 1)),
+    new Quoteclass('The truth is, everyone is going to hurt you. You just got to find the ones worth suffering for.', ' Bob Marley', 'Derrick Maina',  0, 0, new Date(2022, 3, 27)),
+    new Quoteclass('You have to hurt in order to know. Fall in order to grow. Lose in order to gain. Because most of lifes lessons are learned in pain.', 'J Cole', 'Huria',  0, 0, new Date(2022, 2, 7)),
    
   ];
 
